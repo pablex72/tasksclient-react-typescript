ef178b857a579e51c012759d3885f53429cf3ca9
pablo <tonyp2hoy@gmail.com>
2024-06-15 11:20:56 -0400

    feat: adding style to task list

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ const App = () => {
     <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
       <div className="bg-gray-950 p-4 w-2/5 ">
         <h1 className="text-3xl font-bold text-center block my-2">Tasks APP</h1>
-        <TaskList />
         <TaskForm />
+        <TaskList />
+        
       </div>
     </div>
   )
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -6,9 +6,18 @@ interface Props {
 
 const TaskItem = ({ task }: Props) => {
   return (
-    <div key={task._id}>
-      <h1>{task.title}</h1>
-      <p>{task.description}</p>
+    <div
+      key={task._id}
+      className=" bg-gray-900 p-2 my-2 flex justify-between hover:bg-gray-800 hover:cursor-pointer"
+    >
+      <div>
+        <h1>{task.title}</h1>
+        <p>{task.description}</p>
+      </div>
+      <div className="flex gap-x-2">
+        <button>update</button>
+        <button>delete</button>
+      </div>
     </div>
   );
 };
